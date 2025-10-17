fb1888b6fd2bb6616784ff44628a1adba90c4e59
Sohee Lee <essen030216@naver.com>
2025-10-17 20:22:35 +0900

    feat(App.js): 사용자 입력 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const userInput = await this.getUserInput();
+    const numbers = this.parseInput(userInput);
+    const sum = this.calculateSum(numbers);
+    this.printResult(sum);
+  }
+
+  async getUserInput() {
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
+    return input;
+  }
+
+  parseInput(userInput) {
+    // 문자열 파싱
+  }
+
+  calculateSum(numbers) {
+    // 합산
+  }
+
+  printResult() {
+    // 결과 출력
+  }
 }
 
 export default App;
