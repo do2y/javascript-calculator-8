f2c012ba621801111d5b08c34744c86105fe231b
Sohee Lee <essen030216@naver.com>
2025-10-18 13:15:00 +0900

    feat(App.js): 문자열 파싱 및 숫자 추출 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console } from '@woowacourse/mission-utils';
+
+const CUSTOM_DELIMITER_START_INDEX = 2;
+const ESCAPED_NEWLINE_LENGTH = 2;
 
 class App {
   async run() {
@@ -9,12 +12,51 @@ class App {
   }
 
   async getUserInput() {
-    const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
     return input;
   }
 
   parseInput(userInput) {
-    // 문자열 파싱
+    const { delimiter, inputWithoutDeclaration } = this.getDelimiter(userInput);
+    return inputWithoutDeclaration.split(delimiter).map((v) => parseInt(v, 10));
+  }
+
+  getDelimiter(userInput) {
+    let delimiter = /[,:]/;
+
+    // 커스텀 구분자가 없는 경우, 기본 구분자를 사용한다.
+    if (!userInput.startsWith('//')) {
+      return { delimiter, inputWithoutDeclaration: userInput };
+    }
+
+    const delimiterEndIndex = userInput.indexOf('\\n');
+
+    if (delimiterEndIndex === -1) {
+      throw new Error('[ERROR] 잘못된 커스텀 구분자 형식입니다.');
+    }
+
+    if (delimiterEndIndex === CUSTOM_DELIMITER_START_INDEX) {
+      throw new Error('[ERROR] 커스텀 구분자가 입력되지 않았습니다.');
+    }
+
+    const customDelimiter = userInput.slice(
+      CUSTOM_DELIMITER_START_INDEX,
+      delimiterEndIndex
+    );
+    if (customDelimiter.length > 1) {
+      throw new Error('[ERROR] 커스텀 구분자는 한 글자만 가능합니다.');
+    }
+
+    if (!isNaN(Number(customDelimiter))) {
+      throw new Error('[ERROR] 커스텀 구분자로 숫자는 사용할 수 없습니다.');
+    }
+
+    const inputWithoutDeclaration = userInput.slice(
+      delimiterEndIndex + ESCAPED_NEWLINE_LENGTH
+    );
+
+    delimiter = new RegExp(`[,:${customDelimiter}]`);
+    return { delimiter, inputWithoutDeclaration };
   }
 
   calculateSum(numbers) {
