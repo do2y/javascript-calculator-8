9431f61185facf57b15aa456d8925dd36f1d849e
Sohee Lee <essen030216@naver.com>
2025-10-19 13:25:26 +0900

    chore(errors.js): 에러 메시지 상수 정의

---
+++ src/errors.js
@@ -0,0 +1,10 @@
+export const ERROR_MESSAGES = {
+  NO_NUMBER: '[ERROR] 숫자가 포함되어야 합니다.',
+  DELIMITER_AT_EDGE: '[ERROR] 구분자는 문자열의 앞이나 뒤에 위치할 수 없습니다.',
+  NEGATIVE_NUMBER: '[ERROR] 음수는 입력할 수 없습니다.',
+  INVALID_TOKEN: '[ERROR] 입력은 숫자와 지정된 구분자(, : 또는 커스텀 구분자)만 가능합니다.',
+  INVALID_CUSTOM_FORMAT: '[ERROR] 잘못된 커스텀 구분자 형식입니다.',
+  EMPTY_CUSTOM: '[ERROR] 커스텀 구분자가 입력되지 않았습니다.',
+  TOO_LONG_CUSTOM: '[ERROR] 커스텀 구분자는 한 글자만 가능합니다.',
+  NUMBER_CUSTOM: '[ERROR] 커스텀 구분자로 숫자는 사용할 수 없습니다.'
+};
